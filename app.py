16769d1c5a215b78ee94ce8bd47f5e590a1f09da
promathus <125613402+promathus@users.noreply.github.com>
2023-02-17 13:01:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
   
 app = Flask(__name__) #creating the Flask class object   
  
-@app.route('/home') #decorator drfines the   
+@app.route('/') #decorator drfines the   
 def home():  
     return "hello, this is our first flask website";  
   
 if __name__ =='__main__':  
-    app.run(host='0.0.0.0')  
+    app.run(host='0.0.0.0',port=81)  
